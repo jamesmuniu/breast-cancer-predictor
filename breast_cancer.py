dc36e135365fd7518812cc9f04a6d04275487f90
James Muniu <jmsmuniu@gmail.com>
2025-04-28 10:28:24 +0300

    Update breast_cancer.py

--- breast_cancer.py
+++ breast_cancer.py
@@ -13,7 +13,7 @@ st.markdown("""
 Breast Cancer Prediction Challenge """)
 st.markdown("<h3 style='font-size: 20px;'>Step by Step Guide.</h3>", unsafe_allow_html=True)
 (""" Download the Dataset""")
-st.markdown(""" Download the dataset from the [UCI Machine Learning Repositor]y(https://archive.ics.uci.edu/dataset/17)
+st.markdown(""" Download the dataset from the [UCI Machine Learning Repository(https://archive.ics.uci.edu/dataset/17)
 """)
 ("""
 Start by downloading the dataset from the UCI Machine Learning Repository . The dataset contains:
