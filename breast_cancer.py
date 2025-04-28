7832a937c69950b3bd5c7f9077db76ea4f30d1f0
James Muniu <jmsmuniu@gmail.com>
2025-04-28 10:30:17 +0300

    Update breast_cancer.py

--- breast_cancer.py
+++ breast_cancer.py
@@ -9,8 +9,6 @@ import seaborn as sns
 
 # Title and description
 st.title("🩺 Breast Cancer Prediction App")
-st.markdown("""
-Breast Cancer Prediction Challenge """)
 st.markdown("<h3 style='font-size: 20px;'>Step by Step Guide.</h3>", unsafe_allow_html=True)
 (""" Download the Dataset""")
 st.markdown(""" Download the dataset from the [UCI Machine Learning Repository(https://archive.ics.uci.edu/dataset/17)
