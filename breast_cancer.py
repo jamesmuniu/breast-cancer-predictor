4205ad517f8c0cdac210eef25584d82fb5ea6d18
James Muniu <jmsmuniu@gmail.com>
2025-04-28 17:57:30 +0300

    Update breast_cancer.py

--- breast_cancer.py
+++ breast_cancer.py
@@ -28,7 +28,7 @@ If you're testing the accuracy of the model:
 Remove the target column (diagnosis) from your dataset before uploading. This column indicates whether each tumor is benign (0) or malignant (1).
 Save the modified dataset as a CSV file .
 Upload Your CSV File
-Use the upload feature on the left to submit your prepared CSV file.
+Use the upload feature on the left of the page 👈to submit your prepared CSV file.
 Ensure the file contains only the 30 feature columns without the target column. """)
 st.markdown("<h3 style='font-size: 20px;'>🔗Run Predictions.</h3>", unsafe_allow_html=True)
 (""" Once uploaded, the pre-trained machine learning model will process your data and generate predictions.
@@ -99,7 +99,7 @@ if uploaded_file is not None:
     except Exception as e:
         st.error(f"An error occurred: {e}")
 else:
-    st.info("Please upload a CSV file on the left to begin, and remember early detection saves lives.")
+    st.info("Please upload a CSV file on the left 👈to begin, and remember early detection saves lives.")
 
 from PIL import Image
 
