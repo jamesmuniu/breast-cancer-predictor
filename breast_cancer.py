b15c8c51f792af713a6033c4a67cea58e5911ce2
James Muniu <jmsmuniu@gmail.com>
2025-04-28 19:57:20 +0300

    Update breast_cancer.py

--- breast_cancer.py
+++ breast_cancer.py
@@ -16,7 +16,7 @@ st.image(image, width=80)
 st.markdown("<h3 style='font-size: 20px;'>🔬🩺 Breast Cancer Prediction App. </h3>", unsafe_allow_html=True)
 
 
-st.markdown("<h3 style='font-size: 20px;'>🔗Step by Step Guide.</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='font-size: 20px;'>🔗Step by Step Guide👣👣👣</h3>", unsafe_allow_html=True)
 (""" Download the Dataset""")
 st.markdown("""Download the dataset from the [UCI Machine Learning Repository(https://archive.ics.uci.edu/dataset/17)
 """)
